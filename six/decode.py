bae7dcb6a194eb75fd4dc8964920fda22cb34457
Gabriel Hofer <leirbagabriel468@gmail.com>
2021-01-19 22:29:41 -0600

    ok

--- six/decode.py
+++ six/decode.py
@@ -7,30 +7,20 @@ import time
 from struct import *
 
 f=open("../../signal.ham","rb")
-#w=open("stream.bin","wb")
+w=open("stream.bin","wb")
 
-code = np.array([ 
-  [0,0], 
-  [1,1], 
-  [0,1], 
-  ])
+#parity=np.concatenate((code,np.identity(3)),axis=1)
 
-parity=np.concatenate((code,np.identity(3)),axis=1)
-
-exit()
-
-
-#print("parity:\n"+str(parity))
 #---------------------------------------------------------------------
 
 def str2mat_vert(s):
-  ret=np.zeros((3,1))
+  ret=np.zeros((5,1))
   for i in range(ret.shape[0]):
     ret[i,0]=int(s[i])
   return ret
 
 def str2mat_horz(s):
-  ret=np.zeros((1,3))
+  ret=np.zeros((1,5))
   for i in range(ret.shape[1]):
     ret[0,i]=int(s[i])
   return ret
@@ -50,26 +40,30 @@ def decode(p):
     # initialize codeword to empty string
     s=''
     # read x IEEE 754 binary16 numbers
-    for j in range(16):
+    for j in range(8):
       # convert 2 python bytes objects to binary string
       binary16=bin(int.from_bytes(b, 'little'))[2:].zfill(16)
       # xor sign bit and append to codeword
-      s+=str(int(binary16[0])^1)
+      s+=str(int(binary16[15])^1)
+
       # repeat 
       b=f.read(2)
       cnt+=1
+    
+    print(s)
+    #w.write(pack('B',int(s,2)))
+    
+    #print(hex(int(s,2)))
+    #check = np.array(list(map(lambda x:x%2, p.dot(str2mat_vert(s)))))
+    #print(np.transpose(check))
 
-    print(s[0:12] + ' ' + s[12:16])
-
-    if cnt>100: break;
+    if cnt>2000: break;
   #return [check,error]
-
-
 
 decode(np.array([]))
   
 f.close()
-#w.close()
+w.close()
 
 
 

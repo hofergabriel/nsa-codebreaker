567a99a1167bce568a87fd82f83bb73fccb8d97e
Gabriel Hofer <leirbagabriel468@gmail.com>
2021-01-27 18:58:48 -0600

    ok

--- six/decode.py
+++ six/decode.py
@@ -3,7 +3,10 @@ Author: Gabriel Hofer
 Date: 01/21/2021
 Useful Commands to Remember: 
 $ od -t x1 stream.bin | head -n 10
+
+number of bytes in signal.ham --> 9633360
 """
+
 
 import numpy as np
 import sys
@@ -11,8 +14,6 @@ import os
 import time
 from struct import *
 
-f=open("../../signal.ham","rb")
-#w=open("stream.bin","wb")
 
 """ /// """
 def hamming_distance(string1, string2):
@@ -23,47 +24,53 @@ def hamming_distance(string1, string2):
 	return dist_counter
 
 """ Read bytes from binary file and decode codewords """
-def decode(p):
-  b=f.read(2)                                                   # read two bytes from file 
-  cnt=int(0)                                                    # initialize cnt and error to zero
-  ret=''
-  while b:
-    s=''                                                        # initialize codeword to empty string
-    for j in range(8):                                          # read x IEEE 754 binary16 numbers
-      binary16=bin(int.from_bytes(b, 'little'))[2:].zfill(16)   # convert 2 python bytes objects to binary string
-      s+=str(int(binary16[15])^1)                                # xor sign bit and append to codeword
-      b=f.read(2) 
-    cnt+=1
-    ret+=s
-    #w.write(pack('B',int(s,2)))
-    if cnt>(1<<8): break;
-  return ret
+def decode(b_sz):
+	b=f.read(2)                                                   # read two bytes from file 
+	cnt=int(0)                                                    # initialize cnt and error to zero
+	ret=''
+	allzeros=True
+	while b:
+		s=''                                                        # initialize codeword to empty string
+		for j in range(b_sz):                                          # read x IEEE 754 binary16 numbers
+			binary16=bin(int.from_bytes(b, 'little'))[2:].zfill(16)   # convert 2 python bytes objects to binary string
 
-""" Call decode function """
-stream = decode(np.array([]))
-print(stream)
+			#binary16 = binary16[0:8] + binary16[8:16]
 
-""" try different codeword lengths && vary codeword size for each block length """
-for i in range(8,64,8):
-  print("-------------------------------------------------")
-  print("blockSize: "+str(i))
-  for k in range(3,i+1):
-    mndist=1e9
-    j=0
-    while j+2*i <len(stream):
-      dist=hamming_distance(stream[j:j+k],stream[j+i:j+i+k])
-      if dist<mndist: mndist=dist
-      j+=i
-    if mndist>1:
-      print("codeword size: "+str(k)+" padding: "+str(i-k)+" mindist: "+str(mndist))
+			s+=str(int(binary16[0])^1)                                # xor sign bit and append to codeword
+			b=f.read(2) 
+		cnt+=1
+		ret+=s
+		#print(s[len(s)-10:len(s)-1])
+		if s[len(s)-1]=='0':
+			allzeros=False
+		#w.write(pack('B',int(s,2)))
+		if cnt>(1<<4): break;
+		#if cnt>(1<<18): break;
+	return allzeros
+
+#w=open("stream.bin","wb")
+
+#stream = decode(8)
+import math
+
+for i in range(10,int(math.sqrt(9633360))+1):
+	f=open("../../signal.ham","rb")
+	if 9633360%i==0:
+		print("i: "+str(i))
+		z1=decode(i)
+		print("allzeros: "+str(z1))
+
+		print("i: "+str(9633360 // i))
+		z2=decode( 9633360 // i)
+		print("allzeros: "+str(z2))
+
+		if z1 or z2:
+			exit()
+	f.close() 
+	print()
 
 
-
-
-
-
-
-
+exit()
 
 
 
@@ -74,10 +81,8 @@ for i in range(8,64,8):
 
 
 """ close files and exit """
-# f.close() ; w.close()
+f.close() ; w.close()
 """ dump hex in terminal """
 # os.system(" echo ; hexdump -C stream.bin | head -n 10 ")
-
-
 
 
